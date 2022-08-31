efca3d133cb0cce7c0f4126fe5dc96c2413cf6b9
Etienne Delclaux <e_delclaux@cartong.org>
2022-08-31 20:14:08 +0200

    version 0.15.rc1

--- WIN_zip.bat
+++ WIN_zip.bat
@@ -10,7 +10,7 @@ SET PLUGIN_DIRECTORY_NAME=micsgeocodeplugin
 
 SET PLUGIN_DIRECTORY=%QGIS_PLUGIN_DIRECTORY%\%PLUGIN_DIRECTORY_NAME%\
 
-SET ZIP_TARGET_DIRECTORY="%cd%"\%PLUGIN_DIRECTORY_NAME%-0.14.zip
+SET ZIP_TARGET_DIRECTORY="%cd%"\%PLUGIN_DIRECTORY_NAME%-0.15.rc1.zip
 
 @REM ##########################################################################
 @REM set paths
